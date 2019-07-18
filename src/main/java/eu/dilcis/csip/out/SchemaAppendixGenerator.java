c17d5f64144d5ca16598952f52631d31e4cf7de2
Carl Wilson <carl@openpreservation.org>
2019-07-18 11:39:11 +0100

    FIX: logo and appendix header.

--- src/main/java/eu/dilcis/csip/out/SchemaAppendixGenerator.java
+++ src/main/java/eu/dilcis/csip/out/SchemaAppendixGenerator.java
@@ -77,8 +77,14 @@ public final class SchemaAppendixGenerator {
 		for (ControlledVocabulary vocab : this.vocabs) {
 			handler.nl();
 			handler.emit(GitHubMarkdownFormatter.h3(vocab.name));
+			handler.emit("  ");
+			handler.nl();
 			handler.emit(pandocLink(vocab.id));
+			handler.emit("  ");
+			handler.nl();
 			handler.emit(GitHubMarkdownFormatter.anchor(vocab.id));
+			handler.emit("  ");
+			handler.nl();
 			handler.emit(headString("Maintained By:", vocab.maintenanceAgency));
 			handler.emit("  ");
 			handler.nl();
